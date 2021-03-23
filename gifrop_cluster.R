75556ac4dd75421246c9e25e272a9b39be23bd85
jtrachsel <julestrachsel@gmail.com>
2021-03-23 08:19:42 -0500

    cleanup, add back in roary outputs with island info annotations

--- gifrop_cluster.R
+++ gifrop_cluster.R
@@ -5,8 +5,6 @@ suppressPackageStartupMessages(library(dplyr, quietly = TRUE, warn.conflicts = F
 suppressPackageStartupMessages(library(tidyr, quietly = TRUE, warn.conflicts = FALSE))
 suppressPackageStartupMessages(library(readr, quietly = TRUE, warn.conflicts = FALSE))
 suppressPackageStartupMessages(library(tibble, quietly = TRUE, warn.conflicts = FALSE))
-# suppressPackageStartupMessages(library(ggplot2, quietly = TRUE, warn.conflicts = FALSE))
-# suppressPackageStartupMessages(library(purrr, quietly = TRUE, warn.conflicts = FALSE))
 suppressPackageStartupMessages(library(igraph))
 library(digest)
 library(proxy)
@@ -35,11 +33,6 @@ dat_mat <- island_genes_PA %>%
   column_to_rownames(var = 'island_ID') %>%
   as.matrix()
 
-# probable sparse matricies....
-# sum(dat_mat > 0)/ (nrow(dat_mat) * ncol(dat_mat)) *100 
-
-
-# apply(dat_mat, 1, digest)
 
 # hash the presence/absence vector for each island.
 Element_hashes <- 
@@ -73,11 +66,9 @@ de_rep_dat_mat <- dat_mat[keepers,]
 
 
 #### IF SWITCH HERE FOR TRAD CLUSTERING VS NEW CLUSTERING?
-# library(vegan)
 print('constructing graph: Nodes are islands, edges are simpson similarity index (AKA overlap coef) between joined nodes')
 
 # jaccard dist? switch to jaccard similarity for edge weights?
-# sim_mat <- (1 - dist(de_rep_dat_mat, method = 'binary')) %>% as.matrix()
 print('calculating simpson similarity AKA overlap coefficient')
 sim_mat <- 
   proxy::simil(de_rep_dat_mat, method = 'simpson') %>% 
@@ -86,111 +77,7 @@ sim_mat <-
 
 g <- graph_from_adjacency_matrix(adjmatrix = sim_mat,  weighted = T, mode='upper', diag = F)
 
-############# FIX BELOW HERE #############
-
-# 
-# gpa_gath <- gpa %>%
-#   gather(key='genome', value='locus_tags', -(1:14)) %>%
-#   select(Gene,genome, locus_tags) #mmmk...
-# 
-# # this is a tibble of which locus tags are in each island (along with the island's length)
-# island_ID_loc_tags <- island_info %>%
-#   select(island_ID, locus_tags, island_length) %>%
-#   mutate(locus_tags = strsplit(x = locus_tags, split='|', fixed=TRUE)) %>%
-#   unnest(cols = locus_tags)
-# 
-# # 
-# # # island_dat_loc <- '/home/julian.trachsel/VDL/Islands/'
-# # island_dat_loc <- paste(getwd(),'./gifrop_out/my_islands/', sep = '' )
-# 
-# # BUILD GRAPH WITH ISLANDS AS NODES AND EDGES REPRESENTING THE NUMBER OF SHARED GENES
-# # THEN RUN GRAPH BASED CLUSTERING
-# #
-# 
-# # adds island info to the roary gene_presence_absence dataframe
-# gpa_islands <- gpa_gath %>%
-#   separate_rows(locus_tags, sep = '\t') %>%
-#   left_join(island_ID_loc_tags) %>%
-#   filter(!is.na(island_ID))%>%
-#   group_by(Gene) %>%
-#   summarise(num_islands=length(unique(island_ID)),
-#             longest_island=island_ID[which.max(island_length)],
-#             shortest_island=island_ID[which.min(island_length)],
-#             len_longest=island_length[which.max(island_length)],
-#             len_shortest=island_length[which.min(island_length)],
-#             all_islands=list(unique(island_ID)), 
-#             .groups='drop') %>%
-#   
-#   right_join(gpa)
-# 
-# 
-# 
-# 
-# # This writes out two pangenome csvs
-# # SHOULD I WAIT TO DO THIS TILL LATER?
-# # I think I should write this out and then do clustering in a different R script.
-# poi <- gpa_islands %>%
-#   mutate(all_islands = map_chr(all_islands, paste, collapse = '|')) %>%
-#   # write_csv('pan_with_island_info.csv') %>%
-#   filter(num_islands > 0) #%>%
-# # write_csv('pan_only_islands.csv')
-# 
-
-
-# ii <- read_csv('./my_islands/island_info.csv')
-
-
-# poi <- read_csv('./pan_only_islands.csv')
-
-
-# ipa is a matrix describing which genes are present on which islands
-# rows are islands, columns are genes
-
-# 
-# ipa <- poi %>% select(Gene, all_islands) %>%
-#   separate_rows(all_islands, sep = '\\|') %>%
-#   group_by(Gene, all_islands) %>% tally() %>%
-#   ungroup() %>%
-#   spread(key=Gene, value = n, fill = 0) %>%
-#   tibble::column_to_rownames(var = 'all_islands') %>% as.matrix()
-
-#### INSERT COMMUNITY BASED CLUSTERING HERE ####
-# maybe save this to do within primary clusters?
-# comdist <- dist(ipa, method = 'binary')
-# 
-# plot(cmdscale(comdist))
-
-
-###
-
-# if you dont invert this it becomes how many times each gene occurs together in all the islands?
-# USE THIS FOR SEPARATE SUBMODULE ANALYSIS
-# could interpret edges to represent the number of islands that both of the linked genes co-occur within
-# 
-# ipa <- t(ipa)
-# # ipa[1:10,1:10]
-# # after these two steps the rows are genes and the columns are islands
-# # TRUE means that gene is present on that island and FALSE means that gene is not present on that island
-# 
-# #cross product
-# co_mat <- t(ipa) %*% ipa
-# #co_mat is a co-occurance matrix, both rows and columns are islands
-# # numbers indicate how many genes those islands share with eachother
-# 
-# # set diagonal to 0
-# diag(co_mat) <- 0
-
-
-# Create graph from adjacency matrix
-# edge weights are equal to frequency of co-occurrence
-# g <- graph_from_adjacency_matrix(co_mat, mode = "upper", weighted = TRUE)
-
-# Assign nodes weight equal to number of genes?
-# g <- set.vertex.attribute(g, "v_weight", value = colSums(ipa))
-
-# #
-# plot.igraph(g, vertex.label=NA)
-
+ 
 # find clusters in this network
 print('Primary clustering, any islands sharing any gene will be in the same primary cluster')
 clust1 <- clusters(mode='weak', g)
@@ -233,9 +120,6 @@ clust_info <- tibble(island_ID = names(membership(clust1)),
                      quat_cluster = membership(clust4))
   
 ### re-replicate and propogate cluster labels
-
-
-
 clust_info <- 
   dereplication_info %>%
   left_join(clust_info) %>%
@@ -250,85 +134,13 @@ clust_info <-
   unique()
 
 
-
-
-
-
+# output clustered island info.  
 clust_info <- 
   clust_info %>%
   mutate(genome=sub('(.*)_[0-9]+_[0-9]+', '\\1', island_ID)) %>%
   left_join(island_info) %>% 
-  # left_join(island_info) %>%
-  # left_join(island_types) %>%
   mutate(island_type=ifelse(is.na(island_type), 'unknown', island_type)) %>%
   select(island_ID, acc_frag, ends_with('cluster'), everything(), -genome) %>%
   write_csv('./gifrop_out/clustered_island_info.csv')
-
-
-# 
-# # making gpa with island clustering info
-# 
-# gpa_clust <- gpa_gath %>%
-#   left_join(island_ID_loc_tags) %>%
-#   filter(!is.na(island_ID))%>%
-#   left_join(clust_info, by='island_ID') %>%
-#   group_by(Gene) %>%
-#   summarise(num_islands=length(unique(island_ID)),
-#             num_Sclusts=length(unique(secondary_cluster)),
-#             all_islands=list(unique(island_ID)),
-#             Pcluster = unique(primary_cluster),
-#             all_Sclusters=list(unique(secondary_cluster)),
-#             all_Tclusters=list(unique(tertiary_cluster)), 
-#             all_Qclusters=list(unique(quat_cluster)),
-#             .groups='drop') %>%
-#   
-#   right_join(gpa)
-# 
-# 
-# 
-# # this collapses list columns 'all_islands' and 'all_Sclusters' to pipe delimited strings
-# # so that the gpa_clust dataframe can be written to csv
-# 
-# gpa_clust %>%
-#   mutate(all_islands = map_chr(all_islands, paste, collapse = '|'),
-#          all_Sclusters  = map_chr(all_Sclusters , paste, collapse = '|'), 
-#          all_Tclusters = map_chr(all_Tclusters, paste, collapse = '|'), 
-#          all_Qclusters = map_chr(all_Qclusters, paste, collapse = '|')) %>%
-#   write_csv('./gifrop_out/pan_with_island_info.csv') %>%
-#   filter(num_islands > 0) %>%
-#   write_csv('./gifrop_out/pan_only_islands.csv')
-# 
-# 
-# 
-# 
-
-# some simple stats
-
-# what proportion of the pangenome is contained within these islands?
-
-#total genes in pangenome
-# num_tot_genes <- nrow(gpa_clust)
-# 
-# # genes in core genome
-# num_core_genes <- sum(gpa_clust$`No. isolates` == max(gpa_clust$`No. isolates`))
-# 
-# # genes in accessory genome
-# num_accessory_genes <- sum(gpa_clust$`No. isolates` < max(gpa_clust$`No. isolates`))
-# 
-# # acessory genes on islands
-# num_access_on_island <- nrow(gpa_clust %>% filter(`No. isolates` < max(gpa_clust$`No. isolates`)) %>% filter(num_Sclusts > 0))
-# 
-# # acessory genes not on islands
-# num_access_not_on_island <- nrow(gpa_clust %>% filter(`No. isolates` < max(gpa_clust$`No. isolates`)) %>% filter(is.na(num_Sclusts)))
-# 
-# 
-# # % of the pangenome contained within these genomic islands
-# 
-# print('Percent of pan genome contained within genomic islands:')
-# (nrow(gpa_clust %>% filter(num_Sclusts > 0)) / nrow(gpa_clust)) * 100
-# 
-# # 68% of the accessory genome is contained on these islands
-# print('Percent of accessory genome contained within these islands:')
-# (num_access_on_island / num_accessory_genes) * 100
 
 
---
+++ gifrop_pannotate.R
@@ -0,0 +1,100 @@
+library(readr)
+library(dplyr)
+library(tidyr)
+library(purrr)
+
+# adds island info to the roary pangenome files
+
+gff_files <- list.files(path = './gifrop_out/sequence_data/', pattern = 'short.gff', full.names = TRUE)
+
+# This creates a vector of column specifications to be passed to the read_csv function
+# I had trouble with some of the locus tag column types being guessed as logical
+pan_cols <- c('ccciidiiiiciii')
+locus_tag_cols <- rep_len('c', length(gff_files)) %>% paste(sep = '', collapse = '')
+all_cols <- paste(pan_cols, locus_tag_cols, sep = '', collapse = '')
+
+# this is where the gene, presence/absense is read in
+gpa <- read_csv('./gene_presence_absence.csv', col_types = all_cols)
+
+island_info <- read_csv('./gifrop_out/clustered_island_info.csv', col_types = c('ccccccccddddcddlccccccccc'))
+
+
+gpa_gath <- gpa %>%
+  gather(key='genome', value='locus_tags', -(1:14)) %>%
+  select(Gene,genome, locus_tags) #mmmk...
+
+# this is a tibble of which locus tags are in each island (along with the island's length)
+island_ID_loc_tags <- island_info %>%
+  select(island_ID, locus_tags) %>%
+  mutate(locus_tags = strsplit(x = locus_tags, split='|', fixed=TRUE)) %>%
+  unnest(cols = locus_tags)
+ 
+# making gpa with island clustering info
+# 
+gpa_clust <- gpa_gath %>%
+  left_join(island_ID_loc_tags) %>%
+  filter(!is.na(island_ID))%>%
+  left_join(island_info, by='island_ID') %>%
+  group_by(Gene) %>%
+  summarise(num_islands=length(unique(island_ID)),
+            num_Sclusts=length(unique(secondary_cluster)),
+            all_islands=list(unique(island_ID)),
+            Pcluster = unique(primary_cluster),
+            all_Sclusters=list(unique(secondary_cluster)),
+            all_Tclusters=list(unique(tertiary_cluster)),
+            all_Qclusters=list(unique(quat_cluster)),
+            .groups='drop') %>%
+  right_join(gpa)
+
+# 
+# 
+# 
+# this collapses list columns 'all_islands' and 'all_Sclusters' to pipe delimited strings
+# so that the gpa_clust dataframe can be written to csv
+
+gpa_clust %>%
+  mutate(all_islands = map_chr(all_islands, paste, collapse = '|'),
+         all_Sclusters  = map_chr(all_Sclusters , paste, collapse = '|'),
+         all_Tclusters = map_chr(all_Tclusters, paste, collapse = '|'),
+         all_Qclusters = map_chr(all_Qclusters, paste, collapse = '|')) %>%
+  write_csv('./gifrop_out/pan_with_island_info.csv') %>%
+  filter(num_islands > 0) %>%
+  write_csv('./gifrop_out/pan_only_islands.csv')
+#
+
+
+
+
+
+
+
+# some simple stats
+
+# what proportion of the pangenome is contained within these islands?
+
+# total genes in pangenome
+num_tot_genes <- nrow(gpa_clust)
+
+# genes in core genome
+num_core_genes <- sum(gpa_clust$`No. isolates` == max(gpa_clust$`No. isolates`))
+
+# genes in accessory genome
+num_accessory_genes <- sum(gpa_clust$`No. isolates` < max(gpa_clust$`No. isolates`))
+
+# acessory genes on islands
+num_access_on_island <- nrow(gpa_clust %>% filter(`No. isolates` < max(gpa_clust$`No. isolates`)) %>% filter(num_Sclusts > 0))
+
+# acessory genes not on islands
+num_access_not_on_island <- nrow(gpa_clust %>% filter(`No. isolates` < max(gpa_clust$`No. isolates`)) %>% filter(is.na(num_Sclusts)))
+
+
+# % of the pangenome contained within these genomic islands
+
+print('Percent of pan genome contained within genomic islands:')
+(nrow(gpa_clust %>% filter(num_Sclusts > 0)) / nrow(gpa_clust)) * 100
+
+# % of the accessory genome contained on these islands
+print('Percent of accessory genome contained within these islands:')
+(num_access_on_island / num_accessory_genes) * 100
+
+
